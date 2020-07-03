89de1ded304b85085c007600df69e03e88623e7f
Shreedhar Pandey <sridhar.pandey1@gmail.com>
2020-07-03 18:04:37 -0500

    topNews

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NewsapiService } from '../services/newsapi.service';
 
 @Component({
   selector: 'app-tab1',
@@ -6,7 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+  articles: any;
+  constructor(private newsApiService: NewsapiService) {}
 
-  constructor() {}
+  ionViewDidEnter(){
+    this.newsApiService.getNews().subscribe((data)=>{
+      console.log(data)
+      this.articles = data['articles']
+    })
 
+    // this.newsApiService.getCases().subscribe(data=>{
+    //   console.log(data)
+    // })
+    // console.log(this.newsApiService.getCases())
+  }
 }
