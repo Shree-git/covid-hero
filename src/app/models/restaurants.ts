a353e71d714f6c3c1db7bab2006d762b2e58da52
Shreedhar Pandey <sridhar.pandey1@gmail.com>
2020-07-22 00:30:56 -0500

    theme-update

--- src/app/models/restaurants.ts
+++ src/app/models/restaurants.ts
@@ -8,7 +8,8 @@ export const restaurantsData = [{
     "name": "Panda Express",
 
     // string, the full address of the business
-    "address": "801 Monroe St",
+ 
+    "address": "2904 John Howell Dr",
 
     // string, the city
     "city": "Valparaiso",
@@ -17,7 +18,7 @@ export const restaurantsData = [{
     "state": "IN",
 
     // string, the postal code
-    "postal code": "94107",
+    "postal_code": "46385",
 
     // float, latitude
     "latitude": 37.7817529521,
@@ -72,16 +73,16 @@ export const restaurantsData = [{
     "name": "Panera Bread",
 
     // string, the full address of the business
-    "address": "410 Brown St",
+    "address": "2710 Laporte Ave",
 
     // string, the city
-    "city": "Crown Point",
+    "city": "Valparaiso",
 
     // string, 2 character state code, if applicable
     "state": "IN",
 
     // string, the postal code
-    "postal code": "94107",
+    "postal_code": "46383",
 
     // float, latitude
     "latitude": 37.7817529521,
@@ -96,7 +97,7 @@ export const restaurantsData = [{
     "review_count": 1198,
 
     // integer, 0 or 1 for closed or open, respectively
-    "is_open": 1,
+    "is_open": 0,
 
     // object, business attributes to values. note: some attribute values might be objects
     "attributes": {
@@ -133,19 +134,19 @@ export const restaurantsData = [{
     "business_id": "tnhfDv5Il8EaGSXZGiuQGi",
 
     // string, the business's name
-    "name": "Dominos",
+    "name": "Taco Bell",
 
     // string, the full address of the business
-    "address": "120 Union Ave",
+    "address": "2110 Morthland Dr",
 
     // string, the city
-    "city": "Brownsberg",
+    "city": "Valparaiso",
 
     // string, 2 character state code, if applicable
     "state": "IN",
 
     // string, the postal code
-    "postal code": "94107",
+    'postal_code': "46383",
 
     // float, latitude
     "latitude": 37.7817529521,
@@ -191,4 +192,132 @@ export const restaurantsData = [{
         "Sunday": "11:00-18:00",
         "Saturday": "10:00-21:00"
     }
-}]
+},
+{
+    // string, 22 character unique string business id
+    "business_id": "tnhfDv5Il8EaGSXZGiuQGj",
+
+    // string, the business's name
+    "name": "Louie Wingz and Catfish",
+
+    // string, the full address of the business
+    "address": "454 S Greenwich St",
+
+    // string, the city
+    "city": "Valparaiso",
+
+    // string, 2 character state code, if applicable
+    "state": "IN",
+
+    // string, the postal code
+    'postal_code': "46383",
+
+    // float, latitude
+    "latitude": 37.7817529521,
+
+    // float, longitude
+    "longitude": -122.39612197,
+
+    // float, star rating, rounded to half-stars
+    "stars": 4,
+
+    // integer, number of reviews
+    "review_count": 1198,
+
+    // integer, 0 or 1 for closed or open, respectively
+    "is_open": 1,
+
+    // object, business attributes to values. note: some attribute values might be objects
+    "attributes": {
+        "RestaurantsTakeOut": true,
+        "BusinessParking": {
+            "garage": false,
+            "street": true,
+            "validated": false,
+            "lot": false,
+            "valet": false
+        },
+    },
+
+    // an array of strings of business categories
+    "categories": [
+        "Mexican",
+        "Burgers",
+        "Gastropubs"
+    ],
+
+    // an object of key day to value hours, hours are using a 24hr clock
+    "hours": {
+        "Monday": "10:00-21:00",
+        "Tuesday": "10:00-21:00",
+        "Friday": "10:00-21:00",
+        "Wednesday": "10:00-21:00",
+        "Thursday": "10:00-21:00",
+        "Sunday": "11:00-18:00",
+        "Saturday": "10:00-21:00"
+    }
+},
+{
+    // string, 22 character unique string business id
+    "business_id": "tnhfDv5Il8EaGSXZGiuQGk",
+
+    // string, the business's name
+    "name": "Radius",
+
+    // string, the full address of the business
+    "address": "15 E Lincolnway",
+
+    // string, the city
+    "city": "Valparaiso",
+
+    // string, 2 character state code, if applicable
+    "state": "IN",
+
+    // string, the postal code
+    'postal_code': "46383",
+
+    // float, latitude
+    "latitude": 37.7817529521,
+
+    // float, longitude
+    "longitude": -122.39612197,
+
+    // float, star rating, rounded to half-stars
+    "stars": 4.5,
+
+    // integer, number of reviews
+    "review_count": 1198,
+
+    // integer, 0 or 1 for closed or open, respectively
+    "is_open": 1,
+
+    // object, business attributes to values. note: some attribute values might be objects
+    "attributes": {
+        "RestaurantsTakeOut": true,
+        "BusinessParking": {
+            "garage": false,
+            "street": true,
+            "validated": false,
+            "lot": false,
+            "valet": false
+        },
+    },
+
+    // an array of strings of business categories
+    "categories": [
+        "Mexican",
+        "Burgers",
+        "Gastropubs"
+    ],
+
+    // an object of key day to value hours, hours are using a 24hr clock
+    "hours": {
+        "Monday": "10:00-21:00",
+        "Tuesday": "10:00-21:00",
+        "Friday": "10:00-21:00",
+        "Wednesday": "10:00-21:00",
+        "Thursday": "10:00-21:00",
+        "Sunday": "11:00-18:00",
+        "Saturday": "10:00-21:00"
+    }
+},]
--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -310,5 +310,6 @@ export class AuthenticationService {
       return user
     }
 
+   
     
 }
